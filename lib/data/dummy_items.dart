c2c4a51168fc49befec84a0aff7f326568874c99
markiu200 <markiu2007@interia.pl>
2025-06-12 13:02:33 +0200

    [Solution] Adjust code to match suggested solution
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37213746#learning-tools

--- lib/data/dummy_items.dart
+++ lib/data/dummy_items.dart
@@ -1,5 +1,6 @@
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
 
 List<GroceryItem> groceryItems = [
   GroceryItem(
