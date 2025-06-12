4618208bc6a082cbe2375a30a64635fbbe5ee0d6
markiu200 <markiu2007@interia.pl>
2025-06-12 12:27:30 +0200

    [Exercise] Add GroceryItemWidget to display items

---
+++ lib/widgets/grocery_item_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:shopping_list_app/data/categories.dart';
+
+class GroceryItemWidget extends StatelessWidget {
+  const GroceryItemWidget(this.item, {super.key});
+
+  final GroceryItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SizedBox(
+        width: 16,
+        height: 16,
+        child: DecoratedBox(
+          decoration: BoxDecoration(color: categories[item.category]!.color),
+        ),
+      ),
+    );
+  }
+}
