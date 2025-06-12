1cbc59e59cdf949b854cea436a40652ccb45e5ab
markiu200 <markiu2007@interia.pl>
2025-06-12 12:43:14 +0200

    [Exercise] Add key value to ListView builder

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:shopping_list_app/widgets/grocery_item_widget.dart';
 
 class GroceryScreen extends StatelessWidget {
   GroceryScreen({super.key});
 
-  final List<String> groceries = [];
+  final List<GroceryItem> groceries = groceryItems;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: groceries.length,
-      itemBuilder: (context, index) => Text(""),
+      itemBuilder:
+          (context, index) => GroceryItemWidget(
+            groceryItems[index],
+            key: ValueKey(groceryItems[index].id),
+          ),
     );
   }
 }
