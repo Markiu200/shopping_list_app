e97433223fd218d697dd0ea8d26ee21f867d2913
markiu200 <markiu2007@interia.pl>
2025-06-12 08:34:37 +0200

    [Exercise] Moved Categories enum to categories.dart
    Fits better.

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 
-enum Categories {
-  vegetables,
-  fruit,
-  meat,
-  dairy,
-  carbs,
-  sweets,
-  spices,
-  convenience,
-  hygiene,
-  other,
-}
-
 class Category {
   const Category(this.name, this.color);
 
