bcf1c33e65f2c0b90b1277f70c71076c3e2f0c96
markiu200 <markiu2007@interia.pl>
2025-06-19 15:07:48 +0200

    [Exercise] Add "no items" message and ability to remove items
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37213812#learning-tools

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -25,26 +25,65 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    final int expenseIndex = _groceryItems.indexOf(item);
+    setState(() {
+      _groceryItems.remove(item);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _groceryItems.insert(expenseIndex, item);
+            });
+          },
+        ),
+        content: const Text('Expense deleted.'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      mainContent = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder:
+            (ctx, index) => Dismissible(
+              key: ValueKey(_groceryItems[index].id),
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              background: Container(
+                color: Theme.of(context).colorScheme.error.withAlpha(192),
+              ),
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text("${_groceryItems[index].quantity}"),
+              ),
+            ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Groceries"),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder:
-            (ctx, index) => ListTile(
-              title: Text(_groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: _groceryItems[index].category.color,
-              ),
-              trailing: Text("${_groceryItems[index].quantity}"),
-            ),
-      ),
+      body: mainContent,
     );
   }
 }
